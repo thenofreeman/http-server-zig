d9b9f881998a69393c26cf8b810b49d410a1de8a
nolan freeman abbott <nolan@freemanabbott.com>
2025-03-18 15:20:50 -0500

    write Socket struct

--- src/main.zig
+++ src/main.zig
@@ -1,46 +1,27 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
+const std = @import("std");
+const builtin = @import("builtin");
+const net = std.net;
+
+pub const Socket = struct {
+    _address: net.Address,
+    _stream: net.Stream,
+
+    pub fn init() !Socket {
+        const host = [4]u8 { 127, 0, 0, 1 };
+        const port = 3490;
+        const addr = net.Address.initIp4(host, port);
+
+        const socket = try std.posix.socket(
+            addr.any.family,
+            std.posix.SOCK.STREAM,
+            std.posix.IPPROTO.TCP
+        );
+        const stream = net.Stream { .handle = socket };
+
+        return Socket { ._address = addr, ._stream = stream };
+    }
+};
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
 }
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
-
-const std = @import("std");
-
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("http_server_zig_lib");
